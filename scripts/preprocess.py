1b6680f9d82ae296ad3eab41b0240910e3ed4c0d
marshmellow77 <ec2-user@ip-172-16-163-5.ec2.internal>
2021-10-06 10:47:51 +0000

    refactored preprocessing

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -5,22 +5,24 @@ import pandas as pd
 import numpy as np
 import argparse
 import os
+from datasets import load_dataset
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--file-name", type=str)
+    parser.add_argument("--dataset-name", type=str)
+    parser.add_argument("--datasubset-name", type=str)
     parser.add_argument("--model-name", type=str)
     parser.add_argument("--train-ratio", type=float, default=0.8)
     parser.add_argument("--val-ratio", type=float, default=0.1)
-    parser.add_argument("--star-threshold", type=int, default=3)
     args, _ = parser.parse_known_args()
 
     print("Received arguments {}".format(args))
     
-    # read data
-    input_data_path = os.path.join("/opt/ml/processing/input", args.file_name)
-    print("Reading input data from {}".format(input_data_path))
-    df = pd.read_csv(input_data_path)
+    dataset = load_dataset(args.dataset_name, args.datasubset_name)
+    df = pd.DataFrame(dataset['train'])
+    print(f"Number of records before deduplication: {len(df)}")
+    df = df.drop_duplicates()
+    print(f"Number of records after deduplication: {len(df)}")
     
     # split the dataset into train, val, and test
     train, val, test = np.split(df.sample(frac=1), [int(args.train_ratio*len(df)), int((args.train_ratio+args.val_ratio)*len(df))])
